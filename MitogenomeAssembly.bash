ebe626665610588fbd0f4baf6daa1ad0a44e0e84
yufan <wangyufan22@mails.ucas.ac.cn>
2024-11-05 15:27:25 +0800

    Update MitogenomeAssembly.bash

--- MitogenomeAssembly.bash
+++ MitogenomeAssembly.bash
@@ -51,10 +51,19 @@ seqtk sample -s100 ${FILE2} 20000000 > ${OUTPUT2}
 
 4. run MitoZ 
 #参考说明https://gitee.com/CHANyp/MitoZ#63-directory-structure
+#生成的.megahit.result目录中的.megahit.mitogenome.fa文件是线粒体基因组文件，将用于后续分析
 #大约1.5至3G碱基对（bp）足以进行线粒体基因组组装
 source activate mitozEnv
-python3 /home/wangyf/MitoZ/version_2.4-alpha/release_MitoZ_v2.4-alpha/MitoZ.py all --genetic_code 2 --clade Chordata --outprefix Esox_lucius_BF301-02R0001_good_mitoz --thread_number 20 --fastq1 /home/wangyf/wyf/bmkdata/data/Unknown_BF301-02R0001_good_1.fq.gz --fastq2 /home/wangyf/wyf/bmkdata/data/Unknown_BF301-02R0001_good_2.fq.gz --fastq_read_length 150 --insert_size 350 --run_mode 2 --filter_taxa_method 1 --requiring_taxa 'Chordata'
-# --annotation True \
+python3 /home/wangyf/MitoZ/version_2.4-alpha/release_MitoZ_v2.4-alpha/MitoZ.py all \
+--genetic_code 2 --clade Chordata \
+--outprefix Esox_lucius_BF301-02R0001_good_mitoz \
+--thread_number 20 \
+--fastq1 /home/wangyf/wyf/bmkdata/data/Unknown_BF301-02R0001_good_1.fq.gz \
+--fastq2 /home/wangyf/wyf/bmkdata/data/Unknown_BF301-02R0001_good_2.fq.gz \
+--fastq_read_length 150 --insert_size 350 \
+--run_mode 2 \
+--filter_taxa_method 1 \
+--requiring_taxa 'Chordata'
 --species_name Esox_lucius
 
 
@@ -79,3 +88,58 @@ get_organelle_from_reads.py -1 Unknown_BF301-03R0010_good_1.fq.gz -2 Unknown_BF3
 
 ###################################################################################################################################################
 线粒体基因组注释，使用mitofish网页
+
+
+###################################################################################################################################################
+## 建树
+
+#需要软件trimal、macse、translatorx、samtools、seqkit、iqtree
+#如果有的基因缺失，需要将缺失的序列用NNN补上，否则串联时该个体的其他序列也串联不起来，造成个体数据缺失
+# 对每个基因单独进行比对和过滤
+# 因为translatorx运行完毕后会将序列的原顺序打乱，因此需要提前将序列命名为物种名，以便后续用seqkit concat同一物种不同基因的序列串联在一起
+# translatorx和trimal安装在 conda trans 环境下，且需要translatorx这个文件的路径，复制了一份在 home/wangyf/translatorx
+
+cd /home/wangyf/wyf/bmkdata/liyu-alignment/83gene
+
+source activate trans
+cat mito_gene_list | while read gene; do \
+/home/wangyf/translatorx -i ${gene}.fa -o ${gene}.transx -p F -c 2; \ # 按照编码的氨基酸进行序列比对
+java -jar /home/wangyf/macse_v2.06.jar -prog exportAlignment -align ${gene}.transx.nt_ali.fasta -codonForInternalStop NNN -codonForFinalStop --- -gc_def 02; \ # 对终止密码子进行标注，后面是对生成的NT文件建立索引
+trimal -in ${gene}.transx.nt_ali_NT.fasta -out ${gene}.transx.macse.trimal -automated1; \ # 使用trimal进行比对的trimming。先肉眼看一眼，如果比对的不错，这一步可跳过。
+/home/wangyf/translatorx -i ${gene}.transx.macse.trimal -o ${gene}.transx.macse.trimal.transx.fasta -p F -c 2;\ # 修剪后再次进行序列比对
+java -jar /home/wangyf/macse_v2.06.jar -prog exportAlignment -align ${gene}.transx.macse.trimal.transx.fasta.nt_ali.fasta -codonForInternalStop NNN -codonForFinalStop --- -gc_def 02;
+done
+# 注意对生成的比对文件进行检查，比如比对是否有异常，长度是否正常
+#此处需要修改文件里的ID名,将每个基因文件中的每个样本名改成一样的，后面才能串联到一起
+sed -i 's/[_-]ATPase8/ /g' atp8.152samples.transx.nt_ali_NT.fasta
+# 对每个基因重复操作，或想办法改成一个循环
+
+#####
+# 对生成的fasta文件建立索引
+
+conda activate samtools
+cat mito_gene_list| while read gene; do samtools faidx ${gene}.transx.macse.trimal.transx.fasta.nt_ali_NT.fasta; done
+
+# mito_gene_list文件列出线粒体基因名
+cat mito_gene_list| while read gene; do ls ${gene}.transx.macse.trimal.transx.fasta.nt_ali_NT.fasta; done > infile.list
+
+# 将各个基因的alignment串联到一起
+seqkit concat --infile-list infile.list --out-file concat.fasta
+
+# 分partition构建线粒体蛋白编码基因系统发生树
+# 制作一个partition file，参考如下命令，将每个氨基酸的第1个碱基、第2个碱基、第3个碱基分成不同的partition
+cat mito_gene_list | while read gene; do echo ${gene}; head -n 1 ${gene}.transx.macse.trimal.transx.fasta.nt_ali_NT.fasta.fai | awk '{print $2}'; done | paste - - | awk 'BEGIN{tot=0; line=0} {print "DNA, part"line*3+1" = "tot+1"-"tot+$2"\\3\n""DNA, part"line*3+2" = "tot+2"-"tot+$2"\\3\n""DNA, part"line*3+3" = "tot+3"-"tot+$2"\\3";tot=tot+$2;line+=1}' > partition.file
+# 建树
+source activate iqtree
+iqtree -s concat.fasta --seed 6574744 -T 5 -m MFP+MERGE -B 1000 --prefix cdstest -p partition.file
+
+#得到的结果文件中.contree 和.treefile 文件下载下来，用FigTree软件/iTOL网站看
+
+conda activate base
+cat mito_gene_list| while read gene; do ls ${gene}.transx.macse.trimal.transx.fasta.nt_ali_NT.fasta; done > infile.list
+seqkit concat --infile-list infile.list --out-file concat.fasta
+cat mito_gene_list | while read gene; do echo ${gene}; head -n 1 ${gene}.transx.macse.trimal.transx.fasta.nt_ali_NT.fasta.fai | awk '{print $2}'; done | paste - - | awk 'BEGIN{tot=0; line=0} {print "DNA, part"line*3+1" = "tot+1"-"tot+$2"\\3\n""DNA, part"line*3+2" = "tot+2"-"tot+$2"\\3\n""DNA, part"line*3+3" = "tot+3"-"tot+$2"\\3";tot=tot+$2;line+=1}' > partition.file
+cat mito_gene_list | while read gene; do echo ${gene}; head -n 1 ${gene}.transx.nt_ali_NT.fasta.fai | awk '{print $2}'; done | paste - - | awk 'BEGIN{tot=0; line=0} {print "DNA, part"line*3+1" = "tot+1"-"tot+$2"\\3\n""DNA, part"line*3+2" = "tot+2"-"tot+$2"\\3\n""DNA, part"line*3+3" = "tot+3"-"tot+$2"\\3";tot=tot+$2;line+=1}' > partition.file
+
+source activate iqtree
+iqtree -s concat.fasta --seed 6574744 -T 5 -m MFP+MERGE -B 1000 --prefix rna -p partition.file
